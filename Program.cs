3c914fc48c50037e17348dca7401f32b9ef07464
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-28 21:56:09 +0530

    Regex Html

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UC3_RegexHtmlTags
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "<p>The <code>Regex</code> is a compiled representation of a regular expression.</p>";
+            string pattern = @"<[^>]+>";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
