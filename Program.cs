0130207f2241f6b5efe84c2c822f5044fcf84de6
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-28 22:10:53 +0530

    Validate web address

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UC6_ValidateWebAddressFormat
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^(http|http(s)?://)?([\w-]+\.)+[\w-]+[.com|.in|.org]+(\[\?%&=]*)?");
+            Console.WriteLine("Enter a url ");
+            string Website = Console.ReadLine();
+            if (regex.IsMatch(Website))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+            }
+
+        }
+    }
+
+}
