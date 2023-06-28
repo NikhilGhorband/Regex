c0c2114dbf4813d8b009fe9c99d009aa1e6df0f0
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-28 22:05:50 +0530

    ValidFileName

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UC5_RegexValidFileName
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"([^\s]+(\.(?i)(jpe?g|png|gif|bmp))$)");
+
+            Console.WriteLine("Enter filename with image file extension : ");
+            string filename = Console.ReadLine();
+            if (regex.IsMatch(filename))
+            {
+                Console.WriteLine("Valid file name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid file name");
+            }
+        }
+    }
+}
