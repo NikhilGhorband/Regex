7f54ac8233b00f1f1d761b94806f0e25fdd99171
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-28 22:01:51 +0530

    Number Of Occurrences

---
+++ CountPatternOccurences.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_RegexNumberOfOccurences
+{
+    internal class CountPatternOccurences
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UC4_RegexNumberOfOccurences
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = "fox(es)?";
+            string input = "@\"foxes are omnivorous mammals belonging to several genera of the family Canidae fox.\"";
+
+            int count = CountPatternOccurrences(pattern, input);
+            Console.WriteLine("There are {0} occurrences.", count);
+        }
+        static int CountPatternOccurrences(string pattern, string input)
+        {
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            return matches.Count;
+        }
+
+    }
+}
+    
